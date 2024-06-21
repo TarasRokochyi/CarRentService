2640e05f2287f27e85e05a580ff4bb0fc6824e76
Taras Rokochyi <tarrok2005@gmail.com>
2024-06-21 15:02:30 +0300

    implemented unit of work pattern

---
+++ CarRentService/Server/UOF/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using CarRentService.Server.Repositories.Contracts;
+
+namespace CarRentService.Server.UOF
+{
+    public interface IUnitOfWork
+    {
+        ICarRepository CarRepository { get; }
+        IClientRepository ClientRepository { get; }
+        IFineRepository FineRepository { get; }
+        IRegularClientRepository RegularClientRepository { get; }
+        IRentedCarRepository RentedCarRepository { get; }
+
+        Task<int> CompleteAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ CarRentService/Server/UOF/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using CarRentService.Server.Models;
+using CarRentService.Server.Repositories.Contracts;
+
+namespace CarRentService.Server.UOF
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly CarRentServiceContext context;
+        public ICarRepository CarRepository { get; }
+        public IClientRepository ClientRepository { get; }
+        public IFineRepository FineRepository { get; }
+        public IRegularClientRepository RegularClientRepository { get; }
+        public IRentedCarRepository RentedCarRepository { get; }
+
+        public UnitOfWork(
+            ICarRepository carRepository,
+            IClientRepository clientRepository,
+            IFineRepository fineRepository,
+            IRegularClientRepository regularClientRepository,
+            IRentedCarRepository rentedCarRepository)
+        {
+            CarRepository = carRepository;
+            ClientRepository = clientRepository;
+            FineRepository = fineRepository;
+            RegularClientRepository = regularClientRepository;
+            RentedCarRepository = rentedCarRepository;
+        }
+
+        public async Task<int> CompleteAsync(CancellationToken cancellationToken = default)
+        {
+            return await context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
